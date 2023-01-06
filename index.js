4e190282c81e9cd6f0ef8f631a374809a53d200a
Bernardo <bebezanetti@gmail.com>
2023-01-06 17:55:12 -0300

    feat: aulinha

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+
+const extraiLinks = (texto) => {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  let dados = capturas.map(captura => ({[captura[1]]: captura[2]}));
+
+  return dados;
+}
+
 const trataErro = (erro) => {
   throw new Error(chalk.red(erro.code, ' não arquivo no diretorio'));
 }
 
-// const pegaArquivo = (caminhoArquivo) => {
-//   const encode = 'utf-8';
-//   fs.promises.readFile(caminhoArquivo, encode).then(texto => console.log(texto)).catch(trataErro);
-// }
 
 const pegaArquivo = async (caminhoArquivo) => {
   try {
     const encode = 'utf-8';
     const texto = await fs.promises.readFile(caminhoArquivo, encode);
-    console.log(texto);
+    console.log(extraiLinks(texto))
   } catch (error) {
     trataErro(error)
   } finally { // finally executa sempre no fim, independente!
-    console.log(chalk.bgMagenta('forget'));
+    // console.log(chalk.bgMagenta('forget'));
   }
 }
 
